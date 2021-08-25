c3132419cd9d93604a948c88a0ad54a1c3b2985b
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-26 02:02:48 +0530

    Main Activity Completed.

--- app/src/main/java/com/amostrone/akash/imageeditor/MainActivity.java
+++ app/src/main/java/com/amostrone/akash/imageeditor/MainActivity.java
@@ -1,14 +1,112 @@
 package com.amostrone.akash.imageeditor;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
+import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
 
 public class MainActivity extends AppCompatActivity {
+    
+    Button btPick;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        btPick = findViewById(R.id.bt_pick);
+        imageView = findViewById(R.id.image_view);
+        
+        btPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPermission();
+            }
+        });
+    }
+
+    private void checkPermission() {
+        int permission = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            pickImage();
+        }
+        else{
+            if(permission!= PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        100);
+            }
+            else {
+                pickImage();
+            }
+        }
+    }
+
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==100 && grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            pickImage();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Please grant permission", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            Uri uri = data.getData();
+            switch (requestCode){
+                case 100:
+                    Intent intent = new Intent(MainActivity.this, DsPhotoEditorActivity.class);
+                    intent.setData(uri);
+                    //Output Directory Name
+                    intent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY,
+                            "Images");
+                    //Set toolbar color
+                    intent.putExtra(DsPhotoEditorConstants.DS_TOOL_BAR_BACKGROUND_COLOR,
+                            Color.parseColor("#FF6200EE"));
+                    //Set Background Color
+                    intent.putExtra(DsPhotoEditorConstants.DS_MAIN_BACKGROUND_COLOR,
+                            Color.parseColor("#FFFFFF"));
+                    //Hide Tools
+                    intent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE,
+                            new int[]{DsPhotoEditorActivity.TOOL_WARMTH,
+                            DsPhotoEditorActivity.TOOL_PIXELATE});
+
+                    startActivityForResult(intent,101);
+                    break;
+                case 101:
+                    imageView.setImageURI(uri);
+
+                    Toast.makeText(getApplicationContext(), "Photo Saved", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
     }
 }
